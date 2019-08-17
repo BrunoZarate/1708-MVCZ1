b385205d8d1bab92493393d0529c433079e4a8d7
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 10:07:15 -0400

    Solucion

---
+++ MVCProject/MVCProject/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MVCProject.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCProject.Models.Product> Products { get; set; }
+    }
+}
